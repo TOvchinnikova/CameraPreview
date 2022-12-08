6e284fa2fa91202948ca868bf96d365e98aedeaf
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2022-12-08 13:58:24 +0500

    Refactoring ViewFinderOverlay

--- app/src/main/java/com/t_ovchinnikova/android/camerapreview/ui/theme/CameraPreview.kt
+++ app/src/main/java/com/t_ovchinnikova/android/camerapreview/ui/theme/CameraPreview.kt
@@ -4,17 +4,12 @@ import android.util.Log
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import com.t_ovchinnikova.android.camerapreview.ViewFinderOverlay
 import com.t_ovchinnikova.android.camerapreview.getCameraProvider
 import kotlinx.coroutines.launch
@@ -61,8 +56,9 @@ fun CameraPreview(
             previewView
         }
     )
-    AndroidView(factory = {
-        ViewFinderOverlay(context = it, attrs = null)
-    }
+    AndroidView(
+        factory = {
+            ViewFinderOverlay(context = it, attrs = null)
+        }
     )
 }

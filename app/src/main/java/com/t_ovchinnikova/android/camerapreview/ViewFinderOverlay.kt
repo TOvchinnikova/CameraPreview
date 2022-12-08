9e80722b9818798b23e76fe729e2226d28246fc0
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2022-12-08 13:56:18 +0500

    Refactoring ViewFinderOverlay

--- app/src/main/java/com/t_ovchinnikova/android/camerapreview/ViewFinderOverlay.kt
+++ app/src/main/java/com/t_ovchinnikova/android/camerapreview/ViewFinderOverlay.kt
@@ -1,47 +1,114 @@
 package com.t_ovchinnikova.android.camerapreview
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
+import kotlin.properties.Delegates
 
-class ViewFinderOverlay(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+class ViewFinderOverlay(
+    context: Context,
+    attrs: AttributeSet?
+) : View(context, attrs) {
 
-    private val boxPaint: Paint = Paint().apply {
-        color = Color.WHITE
-        style = Paint.Style.STROKE
-        strokeWidth = 1.toFloat()
+    private var strokeColor by Delegates.notNull<Int>()
+    private var backgroundRecticle by Delegates.notNull<Int>()
+    private var strokeBoxWidth by Delegates.notNull<Float>()
+    private var boxCornerRadius by Delegates.notNull<Float>()
+
+    private var desiredWidthCropPercent by Delegates.notNull<Int>()
+    private var desiredHeightCropPercent by Delegates.notNull<Int>()
+
+    private val boxPaint by lazy {
+        Paint().apply {
+            style = Paint.Style.STROKE
+            strokeWidth = strokeBoxWidth
+            color = strokeColor
+        }
     }
 
-    @SuppressLint("ResourceType")
-    private val scrimPaint: Paint = Paint().apply {
-        color = Color.BLACK
+    private val scrimPaint by lazy {
+        Paint().apply {
+            color = backgroundRecticle
+        }
     }
 
-    private val eraserPaint: Paint = Paint().apply {
-        strokeWidth = boxPaint.strokeWidth
-        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+    private val eraserPaint by lazy {
+        Paint().apply {
+            strokeWidth = boxPaint.strokeWidth
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        }
     }
-
-    private val boxCornerRadius: Float =
-        8.toFloat()
 
     private var boxRect: RectF? = null
 
+    init {
+        if (attrs != null) {
+            initAttributes(attrs)
+        } else {
+            initDefaultAttrsValues()
+        }
+    }
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+
         setViewFinder()
     }
 
-    fun setViewFinder() {
+    private fun initAttributes(attributesSet: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(
+            attributesSet,
+            R.styleable.ViewFinderOverlay
+        )
+
+        strokeColor =
+            typedArray.getColor(
+                R.styleable.ViewFinderOverlay_strokeColor,
+                STROKE_DEFAULT_COLOR
+            )
+        backgroundRecticle = typedArray.getColor(
+            R.styleable.ViewFinderOverlay_backgroundRecticle,
+            ContextCompat.getColor(context, BACKGROUND_RECTICLE_DEFAULT_COLOR)
+        )
+        strokeBoxWidth = typedArray.getFloat(
+            R.styleable.ViewFinderOverlay_strokeBoxWidth,
+            STROKE_BOX_WIDTH_DEFAULT
+        )
+        boxCornerRadius = typedArray.getFloat(
+            R.styleable.ViewFinderOverlay_boxCornerRadius,
+            BOX_CORNER_RADIUS_DEFAULT
+        )
+        desiredHeightCropPercent = typedArray.getInt(
+            R.styleable.ViewFinderOverlay_desiredHeightCropPercent,
+            DESIRED_HEIGHT_CROP_PERCENT_DEFAULT
+        )
+        desiredWidthCropPercent = typedArray.getInt(
+            R.styleable.ViewFinderOverlay_desiredWidthCropPercent,
+            DESIRED_WIDTH_CROP_PERCENT_DEFAULT
+        )
+
+        typedArray.recycle()
+    }
+
+    private fun initDefaultAttrsValues() {
+        strokeColor = STROKE_DEFAULT_COLOR
+        backgroundRecticle = ContextCompat.getColor(context, BACKGROUND_RECTICLE_DEFAULT_COLOR)
+        strokeBoxWidth = STROKE_BOX_WIDTH_DEFAULT
+        boxCornerRadius = BOX_CORNER_RADIUS_DEFAULT
+        desiredHeightCropPercent = DESIRED_HEIGHT_CROP_PERCENT_DEFAULT
+        desiredWidthCropPercent = DESIRED_WIDTH_CROP_PERCENT_DEFAULT
+    }
+
+    private fun setViewFinder() {
         val overlayWidth = width.toFloat()
         val overlayHeight = height.toFloat()
 
-        val rectTop = overlayHeight * DESIRED_HEIGHT_CROP_PERCENT / 2 / 100f
-        val rectLeft = overlayWidth * DESIRED_WIDTH_CROP_PERCENT / 2 / 100f
-        val rectRight = overlayWidth * (1 - DESIRED_WIDTH_CROP_PERCENT / 2 / 100f)
-        val rectBottom = overlayHeight * (1 - DESIRED_HEIGHT_CROP_PERCENT / 2 / 100f)
+        val rectTop = overlayHeight * desiredHeightCropPercent / 2 / 100f
+        val rectLeft = overlayWidth * desiredWidthCropPercent / 2 / 100f
+        val rectRight = overlayWidth * (1 - desiredWidthCropPercent / 2 / 100f)
+        val rectBottom = overlayHeight * (1 - desiredHeightCropPercent / 2 / 100f)
         boxRect = RectF(rectLeft, rectTop, rectRight, rectBottom)
 
         invalidate()
@@ -62,7 +129,13 @@ class ViewFinderOverlay(context: Context, attrs: AttributeSet?) : View(context, 
     }
 
     companion object {
-        const val DESIRED_WIDTH_CROP_PERCENT = 20
-        const val DESIRED_HEIGHT_CROP_PERCENT = 74
+        const val DESIRED_WIDTH_CROP_PERCENT_DEFAULT = 20
+        const val DESIRED_HEIGHT_CROP_PERCENT_DEFAULT = 74
+
+        const val BACKGROUND_RECTICLE_DEFAULT_COLOR = R.color.recticle_background
+        const val STROKE_DEFAULT_COLOR = Color.WHITE
+
+        const val STROKE_BOX_WIDTH_DEFAULT = 1f
+        const val BOX_CORNER_RADIUS_DEFAULT = 10f
     }
 }
